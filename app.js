d382be530bebf617d9f78a8658a33d75f51e7261
aflorin <ats@gmail.com>
2017-11-14 21:35:27 +0200

    Set wiredep.

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
-   res.send('Hello World!'); 
+    res.send('Hello World!');
 });
 
 app.get('/books', function (req, res) {
-   res.send('Hello Books!');
+    res.send('Hello Books!');
 });
-
 
 app.listen(port, function (err) {
     console.log('running server on port ' + port);
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var jsHint = require('gulp-jsHint');
+var jscs = require('gulp-jscs');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function() {
+    return gulp.src(jsFiles)
+        .pipe(jsHint())
+        .pipe(jsHint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib'
+    }
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
+});
