5f2d53ddb7b066e73ddc4468f8a5b6bbb8e6a076
aflorin <ats@gmail.com>
2017-11-15 23:57:08 +0200

    Set EJS templating engine.

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('index', {
-        title: 'Hello from render',
-        list: ['a','b']
+        title: 'Hello from render (ejs)',
+        list: ['a','b', 'c', 'd']
     });
 });
 
