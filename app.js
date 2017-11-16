82eaff19f7a2376457745c554c0ad0de4818fa74
aflorin <ats@gmail.com>
2017-11-16 08:44:35 +0200

    Separate files.

--- app.js
+++ app.js
@@ -4,47 +4,12 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-var bookRouter = express.Router();
+var bookRouter = require('./src/routes/bookRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
-
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'Les Miserables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: true
-    }
-];
-
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', {
-            title: 'Hello from render (ejs)',
-            nav: [{
-                    Link: '/Books',
-                    Text: 'Books'
-                }, {
-                    Link: '/Authors',
-                    Text: 'Authors'
-                }],
-            books: books
-        });
-    });
-
-bookRouter.route('/single')
-    .get(function (req, res) {
-        res.send('Hello Single Book!');
-    });
 
 app.use('/Books', bookRouter);
 
