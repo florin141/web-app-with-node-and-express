261d2454b939496f31b4e9dde5c0032062a06c84
florin141 <29834996+florin141@users.noreply.github.com>
2017-11-19 00:39:27 +0200

    Add bookController.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,51 +1,16 @@
 var express = require('express');
 var bookRouter = express.Router();
-var mongodb = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
 
 var router = function(nav) {
+    var bookController = require('../controllers/bookController')(null, nav);
 
-    bookRouter.use(function (req, res, next) {
-        if (!req.user) {
-            res.redirect('/');
-        }
-        next();
-    });
+    bookRouter.use(bookController.middleware);
 
     bookRouter.route('/')
-        .get(function (req, res) {
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-
-                collection.find({}).toArray(function (mongoError, results) {
-                    res.render('bookListView', {
-                        title: 'Books',
-                        nav: nav,
-                        books: results
-                    });
-                });
-            });
-        });
+        .get(bookController.getIndex);
     
     bookRouter.route('/:id')
-        .get(function (req, res) {
-            var id = new objectId(req.params.id);
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-
-                collection.findOne({_id: id}, function (mongoError, results) {
-                    res.render('bookView', {
-                        title: 'Books',
-                        nav: nav,
-                        book: results
-                    });
-                });
-            });
-        });
+        .get(bookController.getById);
 
         return bookRouter;
 }
