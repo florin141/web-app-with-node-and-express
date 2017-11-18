6ad6c9343f5aa67f0dfe56e585a9abce215783be
florin141 <29834996+florin141@users.noreply.github.com>
2017-11-18 20:19:52 +0200

    Add passport middleware.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -15,12 +19,15 @@ var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
 var authRouter = require('./src/routes/authRoutes')(nav);
 
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.set('views', './src/views');
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
 
+require('./src/config/passport')(app);
+
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
@@ -33,7 +40,6 @@ app.get('/', function (req, res) {
         nav: nav
     });
 });
-
 app.get('/books', function (req, res) {
     res.send('Hello Books!');
 });
